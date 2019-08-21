7e8f84da55be374d71cbd39481412acb788a89cc
Michael Rüttgers <michael@ruettgers.eu>
2019-08-21 21:00:54 +0200

    Removed qemu crossbuild dependency, we have a mac again :-)

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ set -xe
 docker build -t mruettgers/rpi-shairport-sync -f targets/rpi/Dockerfile .
 
 # Build amd64 image
-docker build -t mruettgers/shairport-sync -f targets/amd64/Dockerfile .
+#docker build -t mruettgers/shairport-sync -f targets/amd64/Dockerfile .
